7f52287c7b57e8e10dfca638655943a17e1fcd95
grindpunk <clmarijuana@gmail.com>
2026-03-30 11:46:42 +0700

    ^update migrations

---
+++ database/migrations/2026_03_30_044405_create_tb_park_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tb_park', function (Blueprint $table) {
+            $table->id('id_park');
+            $table->string('park_name', 50);
+            $table->text('article_title');
+            $table->text('article_desc');
+            $table->text('park_img')->nullable();
+            $table->string('park_category', 50);
+            $table->string('park_code', 50)->unique();
+            $table->integer('status_park')->default(1);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_park');
+    }
+};
